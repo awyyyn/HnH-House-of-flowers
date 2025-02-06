049b40d1f5b850445fe459871b6ef87f90df74c5
awynx <alwin.puche16@gmail.com>
2025-02-06 22:53:12 +0800

    add Skeleton, Collapsible, Tooltip, Separator components; implement useIsMobile hook; update AdminLayout and Dashboard page with new components

--- client/src/layouts/admin-layout.tsx
+++ client/src/layouts/admin-layout.tsx
@@ -1,9 +1,45 @@
+import {
+	AppSidebar,
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+	Separator,
+} from "@/components";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
 
 export default function AdminLayout() {
 	return (
-		<div>
-			<Outlet />
-		</div>
+		<SidebarProvider>
+			<AppSidebar />
+			<main className="container mx-auto bg-gray-100 w-full relative max-h-[100dvh] h-screen ">
+				<header className="flex h-16 sticky top-0 backdrop-blur-md bg-white/40 shrink-0 items-center gap-2">
+					<div className="flex items-center gap-2 px-4">
+						<SidebarTrigger className="md:hidden block -ml-1" />
+						<Separator
+							orientation="vertical"
+							className="md:hidden block mr-2 h-4"
+						/>
+						<Breadcrumb>
+							<BreadcrumbList>
+								<BreadcrumbItem className="hidden md:block">
+									<BreadcrumbLink href="#">
+										Building Your Application
+									</BreadcrumbLink>
+								</BreadcrumbItem>
+								<BreadcrumbSeparator className="hidden md:block" />
+								<BreadcrumbItem>
+									<BreadcrumbPage>Data Fetching</BreadcrumbPage>
+								</BreadcrumbItem>
+							</BreadcrumbList>
+						</Breadcrumb>
+					</div>
+				</header>
+				<Outlet />
+			</main>
+		</SidebarProvider>
 	);
 }
