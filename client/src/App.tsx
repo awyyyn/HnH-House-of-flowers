224164a0e90690f42cf3bad76e235930004f65b1
awynx <alwin.puche16@gmail.com>
2025-08-20 10:32:38 +0800

    Comment out Customize route and skeleton from user routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,25 +19,25 @@ import PoliciesPage from "./app/policies";
 
 import loadable from "@loadable/component";
 import {
-	DashboardSkeleton,
-	MessagingSkeletonLayoutLoading,
-	ConversationSkeletonLoading,
-	UsersSkeleton,
-	AddAdminSkeleton,
-	SystemSettingsSkeleton,
-	AddOrderSkeleton,
-	AdminOrdersSkeleton,
-	BouquetItemsSkeleton,
-	AddBouquetItemSkeleton,
-	AddProductSkeleton,
-	ProductInfoSkeleton,
-	CardSkeleton,
-	CartSkeleton,
-	UserOrdersSkeleton,
-	ReviewSkeleton,
-	CustomizeSkeleton,
-	CheckoutSkeleton,
-	HomeSkeleton,
+  DashboardSkeleton,
+  MessagingSkeletonLayoutLoading,
+  ConversationSkeletonLoading,
+  UsersSkeleton,
+  AddAdminSkeleton,
+  SystemSettingsSkeleton,
+  AddOrderSkeleton,
+  AdminOrdersSkeleton,
+  BouquetItemsSkeleton,
+  AddBouquetItemSkeleton,
+  AddProductSkeleton,
+  ProductInfoSkeleton,
+  CardSkeleton,
+  CartSkeleton,
+  UserOrdersSkeleton,
+  ReviewSkeleton,
+  // CustomizeSkeleton,
+  CheckoutSkeleton,
+  HomeSkeleton,
 } from "./app/skeletons";
 
 const Dashboard = loadable(() => import("./app/admin/dashboard"));
@@ -65,294 +65,294 @@ const Bouquets = loadable(() => import("./app/user/bouquets"));
 const CartPage = loadable(() => import("./app/user/cart"));
 const UserOrders = loadable(() => import("./app/user/orders"));
 const ReviewPage = loadable(() => import("./app/user/review"));
-const Customize = loadable(() => import("./app/user/customize"));
+// const Customize = loadable(() => import("./app/user/customize"));
 const CheckoutPage = loadable(() => import("./app/user/checkout"));
 const Home = loadable(() => import("./app/home"));
 
 export default function App() {
-	const publicRoutes = [
-		{
-			path: "/",
-			element: <UserLayout />,
-			children: [
-				{ index: true, element: <Home fallback={<HomeSkeleton />} /> },
-			],
-		},
-		{
-			path: "unauthorized",
-			element: <Unauthorized />,
-		},
-		{
-			path: "*",
-			element: <NotFound />,
-		},
-	];
+  const publicRoutes = [
+    {
+      path: "/",
+      element: <UserLayout />,
+      children: [
+        { index: true, element: <Home fallback={<HomeSkeleton />} /> },
+      ],
+    },
+    {
+      path: "unauthorized",
+      element: <Unauthorized />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ];
 
-	const adminRoutes = {
-		element: <AdminLayout />,
-		children: [
-			{
-				element: <ProtectedRoute allowedRoles={["ADMIN", "SUPER_ADMIN"]} />,
-				children: [
-					{
-						element: <Dashboard fallback={<DashboardSkeleton />} />,
-						path: "dashboard",
-					},
-					{
-						element: <Settings fallback={<SystemSettingsSkeleton />} />,
-						path: "settings",
-					},
-					{
-						path: "users",
-						element: <UsersPage fallback={<UsersSkeleton />} />,
-					},
-					{
-						path: "orders",
-						children: [
-							{
-								path: "add",
-								element: <AddOrder fallback={<AddOrderSkeleton />} />,
-							},
-							{
-								index: true,
-								element: <Orders fallback={<AdminOrdersSkeleton />} />,
-							},
-							{
-								path: "pre-orders",
-								element: <PreOrders fallback={<AdminOrdersSkeleton />} />,
-							},
-						],
-					},
-					{
-						path: "messages",
-						element: <Messages fallback={<MessagingSkeletonLayoutLoading />} />,
-						children: [
-							{
-								index: true,
-								element: <MessagesMobile />,
-							},
-							{
-								path: ":userId",
-								element: (
-									<Conversation fallback={<ConversationSkeletonLoading />} />
-								),
-							},
-						],
-					},
-					{
-						path: "bouquet-items",
-						children: [
-							{
-								index: true,
-								element: <BouquetItems fallback={<BouquetItemsSkeleton />} />,
-							},
-							{
-								path: "add-item",
-								element: (
-									<AddBouquetItem fallback={<AddBouquetItemSkeleton />} />
-								),
-							},
-						],
-					},
-					{
-						path: "products",
-						children: [
-							{
-								index: true,
-								element: <Products fallback={<AdminOrdersSkeleton />} />,
-							},
-							{
-								path: "add-product",
-								element: <AddProduct fallback={<AddProductSkeleton />} />,
-							},
-							{
-								path: "edit/:id",
-								element: <EditProduct fallback={<AddProductSkeleton />} />,
-							},
-							{
-								path: ":productId/info",
-								element: <ProductInfo fallback={<ProductInfoSkeleton />} />,
-							},
-						],
-					},
-				],
-			},
-			{
-				element: <ProtectedRoute allowedRoles={["SUPER_ADMIN"]} />,
-				children: [
-					{
-						path: "admins",
-						children: [
-							{
-								index: true,
-								element: <AdminsPage fallback={<AdminOrdersSkeleton />} />,
-							},
-							{
-								path: "add-admin",
-								element: <AddAdmin fallback={<AddAdminSkeleton />} />,
-							},
-						],
-					},
-				],
-			},
-		],
-	};
+  const adminRoutes = {
+    element: <AdminLayout />,
+    children: [
+      {
+        element: <ProtectedRoute allowedRoles={["ADMIN", "SUPER_ADMIN"]} />,
+        children: [
+          {
+            element: <Dashboard fallback={<DashboardSkeleton />} />,
+            path: "dashboard",
+          },
+          {
+            element: <Settings fallback={<SystemSettingsSkeleton />} />,
+            path: "settings",
+          },
+          {
+            path: "users",
+            element: <UsersPage fallback={<UsersSkeleton />} />,
+          },
+          {
+            path: "orders",
+            children: [
+              {
+                path: "add",
+                element: <AddOrder fallback={<AddOrderSkeleton />} />,
+              },
+              {
+                index: true,
+                element: <Orders fallback={<AdminOrdersSkeleton />} />,
+              },
+              {
+                path: "pre-orders",
+                element: <PreOrders fallback={<AdminOrdersSkeleton />} />,
+              },
+            ],
+          },
+          {
+            path: "messages",
+            element: <Messages fallback={<MessagingSkeletonLayoutLoading />} />,
+            children: [
+              {
+                index: true,
+                element: <MessagesMobile />,
+              },
+              {
+                path: ":userId",
+                element: (
+                  <Conversation fallback={<ConversationSkeletonLoading />} />
+                ),
+              },
+            ],
+          },
+          {
+            path: "bouquet-items",
+            children: [
+              {
+                index: true,
+                element: <BouquetItems fallback={<BouquetItemsSkeleton />} />,
+              },
+              {
+                path: "add-item",
+                element: (
+                  <AddBouquetItem fallback={<AddBouquetItemSkeleton />} />
+                ),
+              },
+            ],
+          },
+          {
+            path: "products",
+            children: [
+              {
+                index: true,
+                element: <Products fallback={<AdminOrdersSkeleton />} />,
+              },
+              {
+                path: "add-product",
+                element: <AddProduct fallback={<AddProductSkeleton />} />,
+              },
+              {
+                path: "edit/:id",
+                element: <EditProduct fallback={<AddProductSkeleton />} />,
+              },
+              {
+                path: ":productId/info",
+                element: <ProductInfo fallback={<ProductInfoSkeleton />} />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        element: <ProtectedRoute allowedRoles={["SUPER_ADMIN"]} />,
+        children: [
+          {
+            path: "admins",
+            children: [
+              {
+                index: true,
+                element: <AdminsPage fallback={<AdminOrdersSkeleton />} />,
+              },
+              {
+                path: "add-admin",
+                element: <AddAdmin fallback={<AddAdminSkeleton />} />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
 
-	const authRoutes = {
-		path: "auth",
-		element: <AuthLayout />,
-		children: [
-			{
-				index: true,
-				path: "login",
-				element: <Login />,
-			},
-			{
-				path: "register",
-				element: <Register />,
-			},
-			{
-				path: "forgot-password",
-				element: <ForgotPassword />,
-			},
-		],
-	};
+  const authRoutes = {
+    path: "auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "forgot-password",
+        element: <ForgotPassword />,
+      },
+    ],
+  };
 
-	const userRoutes = {
-		element: <UserLayout />,
-		children: [
-			{
-				element: <ProtectedRoute allowedRoles={["USER"]} />,
-				children: [
-					{
-						path: "customize",
-						element: <Customize fallback={<CustomizeSkeleton />} />,
-					},
-					{
-						path: "add-review/:id",
-						element: <ReviewPage fallback={<ReviewSkeleton />} />,
-					},
-					{
-						path: "my-orders",
-						element: <UserOrders fallback={<UserOrdersSkeleton />} />,
-					},
-					{
-						path: "checkout",
-						children: [
-							{
-								index: true,
-								element: <CheckoutPage fallback={<CheckoutSkeleton />} />,
-							},
-							{
-								path: "success",
-								element: <CheckoutSuccess />,
-							},
-							{
-								path: "error",
-								element: <CheckoutError />,
-							},
-						],
-					},
-					{
-						path: "chat",
-						element: <Chat fallback={<ConversationSkeletonLoading />} />,
-					},
-					{
-						path: "chocolates",
-						children: [
-							{
-								index: true,
-								element: <Chocolates fallback={<CardSkeleton />} />,
-							},
-							{
-								path: ":productId",
-								element: <ProductDetails fallback={<ProductInfoSkeleton />} />,
-							},
-						],
-					},
-					{
-						path: "flowers",
-						children: [
-							{
-								index: true,
-								element: <Flowers fallback={<CardSkeleton />} />,
-							},
-							{
-								path: ":productId",
-								element: <ProductDetails fallback={<ProductInfoSkeleton />} />,
-							},
-						],
-					},
-					{
-						path: "gifts",
-						children: [
-							{
-								index: true,
-								element: <Gifts fallback={<CardSkeleton />} />,
-							},
-							{
-								path: ":productId",
-								element: <ProductDetails fallback={<ProductInfoSkeleton />} />,
-							},
-						],
-					},
-					{
-						path: "bouquets",
-						children: [
-							{
-								index: true,
-								element: <Bouquets fallback={<CardSkeleton />} />,
-							},
-							{
-								path: ":productId",
-								element: <ProductDetails fallback={<ProductInfoSkeleton />} />,
-							},
-						],
-					},
-					{
-						path: "cart",
-						element: <CartPage fallback={<CartSkeleton />} />,
-					},
-				],
-			},
-		],
-	};
+  const userRoutes = {
+    element: <UserLayout />,
+    children: [
+      {
+        element: <ProtectedRoute allowedRoles={["USER"]} />,
+        children: [
+          // {
+          // 	path: "customize",
+          // 	element: <Customize fallback={<CustomizeSkeleton />} />,
+          // },
+          {
+            path: "add-review/:id",
+            element: <ReviewPage fallback={<ReviewSkeleton />} />,
+          },
+          {
+            path: "my-orders",
+            element: <UserOrders fallback={<UserOrdersSkeleton />} />,
+          },
+          {
+            path: "checkout",
+            children: [
+              {
+                index: true,
+                element: <CheckoutPage fallback={<CheckoutSkeleton />} />,
+              },
+              {
+                path: "success",
+                element: <CheckoutSuccess />,
+              },
+              {
+                path: "error",
+                element: <CheckoutError />,
+              },
+            ],
+          },
+          {
+            path: "chat",
+            element: <Chat fallback={<ConversationSkeletonLoading />} />,
+          },
+          {
+            path: "chocolates",
+            children: [
+              {
+                index: true,
+                element: <Chocolates fallback={<CardSkeleton />} />,
+              },
+              {
+                path: ":productId",
+                element: <ProductDetails fallback={<ProductInfoSkeleton />} />,
+              },
+            ],
+          },
+          {
+            path: "flowers",
+            children: [
+              {
+                index: true,
+                element: <Flowers fallback={<CardSkeleton />} />,
+              },
+              {
+                path: ":productId",
+                element: <ProductDetails fallback={<ProductInfoSkeleton />} />,
+              },
+            ],
+          },
+          {
+            path: "gifts",
+            children: [
+              {
+                index: true,
+                element: <Gifts fallback={<CardSkeleton />} />,
+              },
+              {
+                path: ":productId",
+                element: <ProductDetails fallback={<ProductInfoSkeleton />} />,
+              },
+            ],
+          },
+          {
+            path: "bouquets",
+            children: [
+              {
+                index: true,
+                element: <Bouquets fallback={<CardSkeleton />} />,
+              },
+              {
+                path: ":productId",
+                element: <ProductDetails fallback={<ProductInfoSkeleton />} />,
+              },
+            ],
+          },
+          {
+            path: "cart",
+            element: <CartPage fallback={<CartSkeleton />} />,
+          },
+        ],
+      },
+    ],
+  };
 
-	const sharedRoutes = [
-		{
-			element: (
-				<ProtectedRoute allowedRoles={["USER", "ADMIN", "SUPER_ADMIN"]} />
-			),
-			children: [
-				{
-					path: "account",
-					element: <Account />,
-				},
-				{
-					path: "notifications",
-					element: <Notification />,
-				},
-			],
-		},
-		{
-			path: "verify-account",
-			element: <VerifyAccount />,
-		},
-		{
-			path: "set-up-account",
-			element: <SetUpAccount />,
-		},
-		{
-			element: <PoliciesPage />,
-			path: "policies",
-		},
-	];
+  const sharedRoutes = [
+    {
+      element: (
+        <ProtectedRoute allowedRoles={["USER", "ADMIN", "SUPER_ADMIN"]} />
+      ),
+      children: [
+        {
+          path: "account",
+          element: <Account />,
+        },
+        {
+          path: "notifications",
+          element: <Notification />,
+        },
+      ],
+    },
+    {
+      path: "verify-account",
+      element: <VerifyAccount />,
+    },
+    {
+      path: "set-up-account",
+      element: <SetUpAccount />,
+    },
+    {
+      element: <PoliciesPage />,
+      path: "policies",
+    },
+  ];
 
-	return useRoutes([
-		adminRoutes,
-		userRoutes,
-		authRoutes,
-		...publicRoutes,
-		...sharedRoutes,
-	]);
+  return useRoutes([
+    adminRoutes,
+    userRoutes,
+    authRoutes,
+    ...publicRoutes,
+    ...sharedRoutes,
+  ]);
 }
