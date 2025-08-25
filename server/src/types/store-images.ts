929d303bfd3074ae73d410d126eb86f3a006b272
awynx <alwin.puche16@gmail.com>
2025-08-25 19:03:06 +0800

    Add ReadStoreImage type with optional pagination

--- server/src/types/store-images.ts
+++ server/src/types/store-images.ts
@@ -4,3 +4,10 @@ export type CreateStoreImageInput = Omit<
   TStoreImage,
   "id" | "createdAt" | "updatedAt"
 >;
+
+export type ReadStoreImage = {
+  pagination?: {
+    page: number;
+    limit: number;
+  };
+};
