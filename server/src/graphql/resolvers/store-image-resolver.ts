c6965f11f0248e4f1bc66d627b08c273bcdc3069
awynx <alwin.puche16@gmail.com>
2025-08-26 09:47:53 +0800

    Add updateStoreImageResolver and improve error handling

--- server/src/graphql/resolvers/store-image-resolver.ts
+++ server/src/graphql/resolvers/store-image-resolver.ts
@@ -1,13 +1,29 @@
 import { GraphQLError } from "graphql";
-import { createStoreImage } from "src/models/store-images-model.js";
-import { CreateStoreImageInput } from "src/types/store-images.js";
+import {
+  createStoreImage,
+  updateStoreImage,
+} from "../../models/store-images-model.js";
+import { CreateStoreImageInput } from "../../types/store-images.js";
 
 export const createStoreImageResolver = async (
   _: never,
   { createstoreImageInput }: { createstoreImageInput: CreateStoreImageInput },
 ) => {
   try {
-    return await createStoreImage(createstoreImageInput);
+    const newStore = await createStoreImage(createstoreImageInput);
+    if (!newStore) throw new GraphQLError("Failed to create store image");
+    return newStore;
+  } catch (error) {
+    throw new GraphQLError((error as GraphQLError).message);
+  }
+};
+
+export const updateStoreImageResolver = async (
+  _: never,
+  { id, data }: { id: string; data: Partial<CreateStoreImageInput> },
+) => {
+  try {
+    return await updateStoreImage(id, data);
   } catch (error) {
     throw new GraphQLError("Internal Server Error!");
   }
