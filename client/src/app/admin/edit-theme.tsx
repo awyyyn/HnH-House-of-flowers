a3ca4426b45a769c47f722e0635852bd243aa556
awynx <alwin.puche16@gmail.com>
2025-08-27 13:31:38 +0800

    Load store image data in EditTheme and pass to form

--- client/src/app/admin/edit-theme.tsx
+++ client/src/app/admin/edit-theme.tsx
@@ -1,7 +1,24 @@
+import { READ_STORE_IMAGE_QUERY } from "@/queries";
+import { StoreImage } from "@/types";
+import { useQuery } from "@apollo/client";
 import * as React from "react";
+import { useParams } from "react-router-dom";
+import CreateThemeForm from "./components/theme-form";
 
 const EditTheme = () => {
-  return <div></div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery<{ storeImage: StoreImage }>(
+    READ_STORE_IMAGE_QUERY,
+    {
+      variables: { id },
+    },
+  );
+
+  if (loading) return <div>Loading...</div>;
+
+  if (!data?.storeImage) return <div>No data found</div>;
+
+  return <CreateThemeForm defaultValue={data.storeImage} />;
 };
 
 export default EditTheme;
